0fe6f9a2620e1d6ed4dc8c0b35aab3aa34c58847
Hilmi Mubarok <m.hilmimubarok@gmail.com>
2026-03-10 19:56:47 +0700

    feat: setup angular routing and clean root component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/login/login';
+import { EmployeeList } from './features/employee-list/employee-list';
+import { EmployeeAdd } from './features/employee-add/employee-add';
+import { EmployeeDetail } from './features/employee-detail/employee-detail';
+
+export const routes: Routes = [
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  { path: 'login', component: Login },
+  { path: 'employee-list', component: EmployeeList },
+  { path: 'employee-add', component: EmployeeAdd },
+
+  { path: 'employee-detail/:username', component: EmployeeDetail },
+
+  { path: '**', redirectTo: '/login' },
+];
